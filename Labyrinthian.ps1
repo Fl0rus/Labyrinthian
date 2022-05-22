6230604146b9938ea892588afeff8eea9f22bac6
Florus vd Rhee <fvdrhee@hotmail.com>
2022-05-22 17:06:10 +0200

    With Sliders

--- Labyrinthian.ps1
+++ Labyrinthian.ps1
@@ -3,8 +3,10 @@ Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-$Global:FrmSizeX =300
+$Global:FrmSizeX =500
 $Global:FrmSizeY =500
+$global:SizeX = 30
+$global:SizeY= 50
 
 Clear-Host
 $FrmLabyrinthian                            = New-Object system.Windows.Forms.Form
@@ -14,34 +16,79 @@ $FrmLabyrinthian.TopMost                    = $true
 $FrmLabyrinthian.BackColor                  = "#000000"
 $FrmLabyrinthian.StartPosition = 'Manual'
 $FrmLabyrinthian.Location                    = New-Object System.Drawing.Point(0,0)
+$FrmLabyrinthian.TransparencyKey = "#888888"
 
 $BtnCreateLabyrinth                         = New-Object system.Windows.Forms.Button
 $BtnCreateLabyrinth.text                    = "Create Labyrinth"
-$BtnCreateLabyrinth.width                   = 200
+$BtnCreateLabyrinth.width                   = 150
 $BtnCreateLabyrinth.height                  = 25
-$BtnCreateLabyrinth.location                = New-Object System.Drawing.Point(50,475)
+$BtnCreateLabyrinth.location                = New-Object System.Drawing.Point(0,0)
 $BtnCreateLabyrinth.Font                    = 'Microsoft Sans Serif,10'
-$BtnCreateLabyrinth.BackColor               = "#888888"
+$BtnCreateLabyrinth.BackColor               = "#999999"
+
+$sldWidth = New-Object System.Windows.Forms.Trackbar
+$sldWidth.width = 100
+$sldWidth.Height = 5
+$sldWidth.location = New-Object System.Drawing.Point(155,0)
+$sldwidth.Maximum = 150
+$sldWidth.Minimum = 5
+$sldWidth.Value = $global:SizeX
+$sldwidth.AutoSize = $True
+$sldwidth.TickStyle = 0
+$sldWidth.Orientation = 0
+$sldWidth.Margin =0
+$sldwidth.Padding =0
+#$sldWidth.BackColor = "000000"
+
+$sldwidthNum = New-Object System.Windows.Forms.NumericUpDown
+$sldwidthNum.width = 45
+$sldwidthNum.Height = 25
+$sldwidthNum.value= $global:SizeY
+$sldwidthNum.Location = New-Object System.Drawing.Point(260,0)
+$sldwidthNum.Maximum =150
+$sldwidthNum.Minimum =5
+
+$sldHeight = New-Object System.Windows.Forms.Trackbar
+$sldHeight.width = 100
+$sldHeight.Height = 5
+$sldHeight.location = New-Object System.Drawing.Point(310,0)
+$sldHeight.Maximum = 150
+$sldHeight.Minimum = 5
+$sldHeight.Value = $global:SizeX
+$sldHeight.AutoSize = $True
+$sldHeight.TickStyle = 0
+$sldHeight.Orientation = 0
+$sldHeight.Margin =0
+$sldHeight.Padding =0
+#$slHeighth.BackColor = "000000"
+
+$sldHeightNum = New-Object System.Windows.Forms.NumericUpDown
+$sldHeightNum.width = 45
+$sldHeightNum.Height = 25
+$sldHeightNum.value= $global:SizeY
+$sldHeightNum.Location = New-Object System.Drawing.Point(415,0)
+$sldHeightNum.Maximum =150
+$sldHeightNum.Minimum =5
+
+$FrmLabyrinthian.controls.AddRange(@($BtnCreateLabyrinth,$sldWidth,$sldwidthNum,$sldHeight,$sldHeightNum))
 
 Function CreateLabyrinth () {
     #Fill labyrinth matrix array
     Clear-Host
     #Draw stuff prep
     [System.Collections.ArrayList]$global:labyrinth =@()
-    $sizex = 30
-    $sizey = 50
-    #$Size = $sizex * $sizey
-    For($i=0;$i -lt $sizex;$i++) {
+    #$Size =  $global:SizeX *  $global:SizeY
+    For($i=0;$i -lt  $global:SizeX;$i++) {
         $global:labyrinth += ,(@()) 
-        For ($o=0;$o -lt $sizey;$o++) {
+        For ($o=0;$o -lt  $global:SizeY;$o++) {
             #$global:labyrinth[$i] += Get-Random -Minimum 0 -Maximum 3
             $global:labyrinth[$i] += 0 #Black Block
         }
     }
-
+    DrawLabyrinth
     #CreateLabyrinth
     $global:labyrinth[0][0] = 0 #start
-    $global:labyrinth[$sizex-1][$sizey-1] = 16 #finish
+    $global:labyrinth[ $global:SizeX-1][ $global:SizeY-1] = 16 #finish
     $x = 0
     $y = 0
     $pointer=0
@@ -55,7 +102,7 @@ Function CreateLabyrinth () {
                 $posDir.Add(@($global:labyrinth[$x][$y-1],$x,($y-1),'u')) #Up
             }
         } #up
-        If ($y -lt ($sizey-1)) {
+        If ($y -lt ( $global:SizeY-1)) {
             If ($global:labyrinth[$x][$y+1] -eq 0 -or $global:labyrinth[$x][$y+1] -ge 16) {
                 $posDir.Add(@(($global:labyrinth[$x][$y+1]),$x,($y+1),'d')) #Down
             }
@@ -65,7 +112,7 @@ Function CreateLabyrinth () {
                 $posDir.Add(@(($global:labyrinth[$x-1][$y]),($x-1),$y,'l')) #left
             }
         }  #left
-        If ($x -lt ($sizex-1)) {
+        If ($x -lt ( $global:SizeX-1)) {
             If($global:labyrinth[$x+1][$y] -eq 0 -or $global:labyrinth[$x+1][$y] -ge 16) {
                 $posDir.Add(@(($global:labyrinth[$x+1][$y]),($x+1),$y,'r'))#right
             }
@@ -97,22 +144,21 @@ Function CreateLabyrinth () {
                 'r' {$value+=4}
             }
             $global:labyrinth[$x][$y] = $value
-            Write-host "Step $pointer = Moved to $x $y"
-            #Draw labyrinth
-            DrawLabyrinth
+            #Write-host "Step $pointer = Moved to $x $y"
         } Else {
             $pointer--
             $x = $moved[$pointer][0]
             $y = $moved[$pointer][1]
-            Write-host "Step $pointer = Niewe scan op punt $x $y"
+            #Write-host "Step $pointer = Niewe scan op punt $x $y"
         }
     }
-
+    #Draw labyrinth
+    DrawLabyrinth
 }
 Function DrawLabyrinth(){    
     $brushw = New-Object Drawing.SolidBrush White
     $brushb = New-Object Drawing.SolidBrush Black
-    #$brushg = New-Object Drawing.SolidBrush Green
+    $brushg = New-Object Drawing.SolidBrush Green
     $brushf = New-Object Drawing.SolidBrush Firebrick
     
     #$pen = New-object Drawing.pen Black
@@ -120,17 +166,19 @@ Function DrawLabyrinth(){
     #Test for drawing
     $offsetx = 20
     $offsety = 25
-    $ScaleX = (($Global:FrmSizeX-($offsetx*2))/$sizex)
-    $ScaleY= (($Global:FrmSizeY-($offsety*2))/$sizey)
+    $Global:FrmSizeX = $FrmLabyrinthian.Width
+    $Global:FrmSizeY = $FrmLabyrinthian.Height - 30
+    $ScaleX = (($Global:FrmSizeX-($offsetx*2))/ $global:SizeX)
+    $ScaleY= (($Global:FrmSizeY-($offsety*2))/ $global:SizeY)
     $RoomSizeX = $ScaleX * 0.75
     $RoomsizeY = $Scaley * 0.75
     
-    For($y=0;$y -lt $sizey;$y++) { 
-        For($x=0;$x -lt $sizex;$x++) {
+    For($y=0;$y -lt  $global:SizeY;$y++) { 
+        For($x=0;$x -lt  $global:SizeX;$x++) {
             Switch($global:labyrinth[$x][$y]) {
-                #0 {
-                #    $graphics.FillRectangle($brushb,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
-                #}
+                0 {
+                    $graphics.FillRectangle($brushb,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
+                }
                 {(1 -band $_) -eq 1} {
                     $graphics.FillRectangle($brushw,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,($RoomSizeX),($RoomsizeY))
                     $graphics.FillRectangle($brushw,($x*$scalex)+$offsetx,(($y-0.25)*$scaleY)+$offsety,($RoomSizeX),($RoomsizeY))
@@ -149,9 +197,12 @@ Function DrawLabyrinth(){
                     $graphics.FillRectangle($brushw,(($x+0.25)*$scalex)+$offsetx,(($y)*$scaleY)+$offsety,($RoomSizeX),($RoomsizeY))
 
                 }
-                default {
-                    $graphics.FillRectangle($brushf,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
+                16 {
+                    $graphics.FillRectangle($brushg,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
                 }
+                #default {
+                #    $graphics.FillRectangle($brushf,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
+                #}
                     
             }
         }
@@ -160,7 +211,18 @@ Function DrawLabyrinth(){
     $FrmLabyrinthian.Update()
 }
 
+function ChangeSizeX () {
+    $sldwidthNum.Value = $sldWidth.Value
+    $global:SizeX = $sldWidth.Value
+}
+function ChangeSizeY () {
+    $sldHeightNum.Value = $sldHeight.Value
+    $global:SizeY = $sldHeight.Value
+}
+
 $BtnCreateLabyrinth.Add_Click({ CreateLabyrinth })
 
-$FrmLabyrinthian.controls.AddRange(@($BtnCreateLabyrinth))
+$sldWidth.Add_Scroll({ ChangeSizeX })
+$sldHeight.Add_Scroll({ ChangeSizeY })
+
 [void][System.Windows.Forms.Application]::Run($FrmLabyrinthian)
