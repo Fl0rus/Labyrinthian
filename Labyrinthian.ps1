e47cda73b4b1297fb277594c15d896c5d0ce4754
Florus vd Rhee <fvdrhee@hotmail.com>
2022-05-22 23:27:24 +0200

    Solving complete

--- Labyrinthian.ps1
+++ Labyrinthian.ps1
@@ -19,12 +19,21 @@ $FrmLabyrinthian.Location                    = New-Object System.Drawing.Point(0
 #$FrmLabyrinthian.TransparencyKey = "#808080"
 
 $BtnCreateLabyrinth                         = New-Object system.Windows.Forms.Button
-$BtnCreateLabyrinth.text                    = "Create Labyrinth"
-$BtnCreateLabyrinth.width                   = 150
+$BtnCreateLabyrinth.text                    = "Create"
+$BtnCreateLabyrinth.width                   = 75
 $BtnCreateLabyrinth.height                  = 25
 $BtnCreateLabyrinth.location                = New-Object System.Drawing.Point(0,0)
 $BtnCreateLabyrinth.Font                    = 'Microsoft Sans Serif,10'
 $BtnCreateLabyrinth.BackColor               = "#999999"
+
+$BtnSolveLabyrinth                         = New-Object system.Windows.Forms.Button
+$BtnSolveLabyrinth.text                    = "Solve"
+$BtnSolveLabyrinth.width                   = 75
+$BtnSolveLabyrinth.height                  = 25
+$BtnSolveLabyrinth.location                = New-Object System.Drawing.Point(75,0)
+$BtnSolveLabyrinth.Font                    = 'Microsoft Sans Serif,10'
+$BtnSolveLabyrinth.BackColor               = "#999999"
+$BtnSolveLabyrinth.Enabled = $false
 
 $sldWidth = New-Object System.Windows.Forms.Trackbar
 $sldWidth.width = 100
@@ -73,13 +82,14 @@ $prgCalc.Height = 20
 $prgcalc.value = 0
 $prgcalc.Location = New-Object System.Drawing.Point(460,0)
 
-$FrmLabyrinthian.controls.AddRange(@($BtnCreateLabyrinth,$sldWidth,$sldwidthNum,$sldHeight,$sldHeightNum,$prgCalc))
+$FrmLabyrinthian.controls.AddRange(@($BtnCreateLabyrinth,$BtnSolveLabyrinth,$sldWidth,$sldwidthNum,$sldHeight,$sldHeightNum,$prgCalc))
 Function CreateLabyrinth () {
     #Fill labyrinth matrix array
     ClearLabyrinth
     #Create Labyrinth
     $x = Get-Random -Minimum 0 -Maximum ($global:SizeX-1)
     $y = Get-Random -Minimum 0 -Maximum ($global:SizeY-1)
+    $Global:Start=@($x,$y)
     $global:labyrinth[$x][$y] = 16 #start
     $pointer=0
     $progress=0
@@ -89,7 +99,6 @@ Function CreateLabyrinth () {
     [System.Collections.ArrayList]$moved = @()
     [System.Collections.ArrayList]$endpoints = @()
     DrawExplorer -x $x -y $y
-    #DrawExplorer -x ($global:SizeX-1) -y ($global:SizeY-1)
     While($pointer -ge 0) {
         [System.Collections.ArrayList]$posDir = @()
         If ($y -gt 0) {
@@ -153,25 +162,26 @@ Function CreateLabyrinth () {
             #Write-host "Step $pointer = Niewe scan op punt $x $y"
         }
     }
+    $BtnSolveLabyrinth.Enabled = $true
     $endpoint=$endpoints[(Get-Random -Minimum 0 -Maximum $endpoints.Count)]
     $global:labyrinth[$endpoint[0]][$endpoint[1]] = 32 #finish
+    $Global:Finish=@($endpoint[0],$endpoint[1])
     DrawExplorer -x ($endpoint[0]) -y ($endpoint[1])
     #Draw labyrinth
-    Write-host $progress $pointermax
     #ClearLabyrinth
     #DrawLabyrinth
 }
 Function InitLabyrinth(){
     #Draw stuff prep
-    [System.Collections.ArrayList]$global:labyrinth =@()
+    [System.Collections.ArrayList]$global:labyrinth = @()
     #$Size =  $global:SizeX *  $global:SizeY
-    For($i=0;$i -lt  $global:SizeX;$i++) {
+    For($i=0;$i -lt $global:SizeX;$i++) {
         $global:labyrinth += ,(@()) 
-        For ($o=0;$o -lt  $global:SizeY;$o++) {
-            #$global:labyrinth[$i] += Get-Random -Minimum 0 -Maximum 3
-            $global:labyrinth[$i] += 0 #Black Block
+        For ($o=0;$o -lt $global:SizeY;$o++) {
+            $global:labyrinth[$i] += 0
         }
     }
+
     $global:Graphics = $FrmLabyrinthian.CreateGraphics()
 
     $prgcalc.width = $FrmLabyrinthian.Width-460
@@ -187,14 +197,16 @@ Function DrawExplorer {
     Param (
         $x,
         $y,
-        [switch]$marker = $false
+        [ValidateSet("Bold", "Normal", "Light")]$marker 
     )
 
     $brushw = New-Object Drawing.SolidBrush White
     $brushb = New-Object Drawing.SolidBrush Black
     $brushg = New-Object Drawing.SolidBrush Green
     $brushf = New-Object Drawing.SolidBrush Firebrick
-    $brushe = New-Object Drawing.SolidBrush Blue
+    $brushb = New-Object Drawing.SolidBrush Blue
+    $brushelb = New-Object Drawing.SolidBrush LightBlue
+    $brushelc = New-Object Drawing.SolidBrush LightCyan
 
     $offsetx = 20
     $offsety = 50
@@ -202,9 +214,12 @@ Function DrawExplorer {
     $ScaleY= ((($FrmLabyrinthian.Height - 25)-($offsety*2))/ $global:SizeY)
     $RoomSizeX = $ScaleX * 0.75
     $RoomsizeY = $Scaley * 0.75
-    If($marker) {
-        $global:Graphics.FillRectangle($brushe,($x*$scalex)+$offsetx,($y*$scaleY+0.25)+$offsety,($RoomSizeX),($RoomsizeY))
-     
+    If($marker -eq 'Bold') {
+        $global:Graphics.FillRectangle($brushb,($x*$scalex)+$offsetx,($y*$scaleY+0.25)+$offsety,($RoomSizeX),($RoomsizeY))
+    } ElseIf($marker -eq 'Normal') {
+        $global:Graphics.FillRectangle($brushelb,($x*$scalex)+$offsetx,($y*$scaleY+0.25)+$offsety,($RoomSizeX),($RoomsizeY))
+    } ElseIf($marker -eq 'Light') {
+        $global:Graphics.FillRectangle($brushelc,($x*$scalex)+$offsetx,($y*$scaleY+0.25)+$offsety,($RoomSizeX),($RoomsizeY))
     } Else {
         $global:Graphics.FillRectangle($brushw,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,($RoomSizeX),($RoomsizeY))
         Switch($global:labyrinth[$x][$y]) {
@@ -264,13 +279,73 @@ function ChangeSizeYNum () {
     $global:SizeY = $sldHeight.Value
 }
 
+Function SolveLabyrinth {
+    Param ()
+    For($i=0;$i -lt  $global:SizeX;$i++) {
+        For ($o=0;$o -lt  $global:SizeY;$o++) {
+            If((($global:labyrinth[$i][$o]) -band 64) -eq 64) {
+                $global:labyrinth[$i][$o]-= 64 #remove breadcrumbs
+            }
+        }
+    }
+    ClearLabyrinth
+    DrawLabyrinth
+    Write-host "$global:start $global:finish"
+    $x =$global:start[0]
+    $y =$global:start[1]
+    $Progress=1
+    $progressmax=1
+    $moves=0
+    [System.Collections.ArrayList]$moved=@()
+    DrawExplorer -x $x -y $y -marker Bold
+    While (($global:labyrinth[$x][$y] -band 32) -ne 32) {
+        [System.Collections.ArrayList]$posDir = @()
+        If (($global:labyrinth[$x][$y] -band 2) -eq 2 -and (($global:labyrinth[$x][($y+1)] -band 64) -ne 64)) {$posdir.add(@($x,($y+1)))}
+        If (($global:labyrinth[$x][$y] -band 8) -eq 8 -and (($global:labyrinth[($x+1)][$y] -band 64) -ne 64)) {$posdir.add(@(($x+1),$y))}
+        If (($global:labyrinth[$x][$y] -band 4) -eq 4 -and (($global:labyrinth[($x-1)][$y] -band 64) -ne 64)) {$posdir.add(@(($x-1),$y))}
+        If (($global:labyrinth[$x][$y] -band 1) -eq 1 -and (($global:labyrinth[$x][($y-1)] -band 64) -ne 64)) {$posdir.add(@($x,($y-1)))}
+        $numofposdir = $posdir.Count
+        $Moves++
+        If ($numofposdir -ne 0) {
+            #Random direction
+            $movechoice = $posDir[(Get-Random -Minimum 0 -Maximum ($numofposdir))]
+            #Directed direction
+            #$movechoice = $posDir[0]
+            $x = $movechoice[0]
+            $y = $movechoice[1]
+            $global:labyrinth[$x][$y]+=64
+            DrawExplorer -x $x -y $y -Marker Bold
+            $moved.Add(@($x,$y))
+            $progress=$moved.count-1
+            #Write-host "New route: " -NoNewline
+        } ElseIf($progress -gt $progressmax) {
+            $progressmax=$Progress+1
+            #Write-host "Dead End: " -NoNewline
+            DrawExplorer -x $x -y $y -Marker Normal
+        } Else {
+            $Progress--
+            $x = $moved[$progress][0]
+            $y = $moved[$progress][1]
+            #Write-host "Backtrack: " -NoNewline
+            DrawExplorer -x $x -y $y -marker Light
+        }
+        #Write-host "$x $y"
+    }
+
+    Write-Host "moves: $moves"
+}
+
 $BtnCreateLabyrinth.Add_Click({ InitLabyrinth;CreateLabyrinth })
+$BtnSolveLabyrinth.Add_Click({ SolveLabyrinth })
 
 $sldWidth.Add_Scroll({ ChangeSizeX })
 $sldHeight.Add_Scroll({ ChangeSizeY })
 $sldwidthNum.Add_ValueChanged({ChangeSizeXNum})
 $sldHeightNum.Add_ValueChanged({ChangeSizeYNum})
 $FrmLabyrinthian.Add_ResizeEnd({ClearLabyrinth;DrawLabyrinth})
+$FrmLabyrinthian.Add_Shown({ClearLabyrinth;DrawLabyrinth;$FrmLabyrinthian.Update()})
 
 InitLabyrinth
+#CreateLabyrinth
+#SolveLabyrinth
 [void][System.Windows.Forms.Application]::Run($FrmLabyrinthian)
