90f16842b831844388561113ea42bccb1d3e401a
Florus vd Rhee <fvdrhee@hotmail.com>
2022-05-22 09:02:47 +0200

    Lab building complete

--- Labyrinthian.ps1
+++ Labyrinthian.ps1
@@ -29,18 +29,75 @@ Function CreateLabyrinth () {
     #Draw labyrinth
 
     #Draw stuff prep
-    [System.Collections.ArrayList]$lab =@()
-    $sizex = 5
-    $sizey = 10
-    $Size =$sizex * $sizey
+    [System.Collections.ArrayList]$global:labyrinth =@()
+    $sizex = 15
+    $sizey = 30
+    $Size = $sizex * $sizey
     For($i=0;$i -lt $sizex;$i++) {
-        $lab += ,(@()) 
+        $global:labyrinth += ,(@()) 
         For ($o=0;$o -lt $sizey;$o++) {
-            $lab[$i] += Get-Random -Minimum 0 -Maximum 3
+            #$global:labyrinth[$i] += Get-Random -Minimum 0 -Maximum 3
+            $global:labyrinth[$i] += 1
         }
     }
+
+    #CreateLabyrinth
+    $global:labyrinth[0][0] = 0 #start
+    $global:labyrinth[$sizex-1][$sizey-1] = 99 #finish
+    $x = 0
+    $y = 0
+    $Dir = New-Object System.Collections.Generic.List[System.Object]
+    $runcount = 0
+    While($global:labyrinth[$x][$y] -lt 99) {
+        $global:labyrinth[$x][$y] = 0
+        If ($y -gt 0) {
+            If ($global:labyrinth[$x][$y-1] -gt 0){
+                $Dir.Add(@($global:labyrinth[$x][$y-1],$x,($y-1),'u'))
+            }
+        } #up
+        If ($y -lt ($sizey-1)) {
+            If ($global:labyrinth[$x][$y+1] -gt 0) {
+                $Dir.Add(@(($global:labyrinth[$x][$y+1]),$x,($y+1),'d'))
+            }
+        } #down
+        If ($x -gt 0) {
+            If ($global:labyrinth[$x-1][$y] -gt 0) {
+                $Dir.Add(@(($global:labyrinth[$x-1][$y]),($x-1),$y,'l'))
+            }
+        }  #left
+        If ($x -lt ($sizex-1)) {
+            If($global:labyrinth[$x+1][$y] -gt 0) {
+                $Dir.Add(@(($global:labyrinth[$x+1][$y]),($x+1),$y,'r'))
+            }
+        } #right
+        #Nu nog random
+        $numofdir = $dir.Count
+        If ($numofdir -eq $runcount) {
+            $direction = @(0)
+            while ($direction[0] -eq 0) {
+                $direction = $dir[$dirchoice]
+                $dirchoice = Get-Random -Minimum 0 -Maximum $numofdir
+            }
+            #Write-host "Afslag genomen op punt $dirchoice"
+        } Else {
+            $dirchoice = Get-Random -Minimum $runcount -Maximum ($numofdir)
+        }
+        $direction = $dir[$dirchoice]
+        $x=$direction[1]
+        $y=$direction[2]
+        $value = $direction[0]
+        #Store memory
+        $runcount=$numofdir
+        #Write-Host "Beweging $x $y $value"
+        DrawLabyrinth
+        #$Dir=$null
+    }
+
+}
+Function DrawLabyrinth(){    
     $brushw = New-Object Drawing.SolidBrush White
     $brushb = New-Object Drawing.SolidBrush Black
+    $brushg = New-Object Drawing.SolidBrush Green
     $brushf = New-Object Drawing.SolidBrush Firebrick
     
     $pen = New-object Drawing.pen Black
@@ -52,9 +109,9 @@ Function CreateLabyrinth () {
     $ScaleY= ($Global:FrmSizeY-($offsety*2))/$sizey
     For($y=0;$y -lt $sizey;$y++) { 
         For($x=0;$x -lt $sizex;$x++) {
-            Switch($lab[$x][$y]) {
+            Switch($global:labyrinth[$x][$y]) {
                 0 {
-                    $graphics.FillRectangle($brushw,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
+                    $graphics.FillRectangle($brushw,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,($ScaleX-1),($scaleY-1))
                 }
                 1 {
                     $graphics.FillRectangle($brushb,($x*$scalex)+$offsetx,($y*$scaleY)+$offsety,$ScaleX,$scaleY)
